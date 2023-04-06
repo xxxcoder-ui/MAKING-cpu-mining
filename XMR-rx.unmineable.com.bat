1e7e73282b7ea244c2a900c1aa8b493208278897
xxxcoder-ui <62079682+xxxcoder-ui@users.noreply.github.com>
2023-04-06 16:01:25 -0700

    Training

---
+++ XMR-rx.unmineable.com.bat
@@ -0,0 +1,2 @@
+xmrig.exe -a rx -o stratum+ssl://rx.unmineable.com:443 -u XMR:42cApzE94VmcnywTZnqi2v2hfMdhx9o9sa3HMQGDtEYt6UJuYew3Y2fDrmGVAQzpWLGuNVW3mf8GSC9FAPRNYrwDGz5hyCD.unmineable_worker_mgpoxq#lmad-gdg2 -p x 
+pause
